10fe16aa09e2e2441acfe484680387f914e2f4e1
soyeon <lupmika@naver.com>
2021-01-14 22:32:42 +0900

    관리자 활동 내용을 저장하는 클래스

---
+++ fourplay/src/vo/HistoryInfo.java
@@ -0,0 +1,60 @@
+package vo;
+
+public class HistoryInfo {
+	private int ah_idx, al_idx, am_idx, cs_idx, nl_idx, ql_idx;
+	private String ah_date, pl_id, ol_id;
+	public int getAh_idx() {
+		return ah_idx;
+	}
+	public void setAh_idx(int ah_idx) {
+		this.ah_idx = ah_idx;
+	}
+	public int getAl_idx() {
+		return al_idx;
+	}
+	public void setAl_idx(int al_idx) {
+		this.al_idx = al_idx;
+	}
+	public int getAm_idx() {
+		return am_idx;
+	}
+	public void setAm_idx(int am_idx) {
+		this.am_idx = am_idx;
+	}
+	public int getCs_idx() {
+		return cs_idx;
+	}
+	public void setCs_idx(int cs_idx) {
+		this.cs_idx = cs_idx;
+	}
+	public int getNl_idx() {
+		return nl_idx;
+	}
+	public void setNl_idx(int nl_idx) {
+		this.nl_idx = nl_idx;
+	}
+	public int getQl_idx() {
+		return ql_idx;
+	}
+	public void setQl_idx(int ql_idx) {
+		this.ql_idx = ql_idx;
+	}
+	public String getAh_date() {
+		return ah_date;
+	}
+	public void setAh_date(String ah_date) {
+		this.ah_date = ah_date;
+	}
+	public String getPl_id() {
+		return pl_id;
+	}
+	public void setPl_id(String pl_id) {
+		this.pl_id = pl_id;
+	}
+	public String getOl_id() {
+		return ol_id;
+	}
+	public void setOl_id(String ol_id) {
+		this.ol_id = ol_id;
+	}
+}
