71b9cb0d070105282e8f7a4a0b51d580c23c3c07
soyeon <lupmika@naver.com>
2021-01-18 02:46:10 +0900

    어드민 주문관리 페이지 시작

---
+++ fourplay/src/admin/action/AOrdListAction.java
@@ -0,0 +1,62 @@
+package admin.action;
+
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+import admin.svc.*;
+import svc.OrdListSvc;
+import vo.*;
+
+public class AOrdListAction implements action.Action {
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("AOrdListAction");
+		request.setCharacterEncoding("utf-8");
+		ActionForward forward = new ActionForward();
+		ArrayList<OrdListInfo> ordList = new ArrayList<OrdListInfo>();
+		
+		int cpage = 1, pcnt, spage, epage, rcnt, bsize = 10, psize = 12;		// 페이징에 필요한 값들을 저장할 변수 선언 및 초기화
+		if (request.getParameter("cpage") != null)			cpage = Integer.parseInt(request.getParameter("cpage"));
+		if (request.getParameter("psize") != null)			psize = Integer.parseInt(request.getParameter("psize"));
+
+		HttpSession session = request.getSession();
+		MemberInfo loginMember = (MemberInfo)session.getAttribute("loginMember");
+		String buyer = "";
+		
+		if(loginMember != null) {	// 로그인한 회원이면
+			buyer = loginMember.getMlid();
+			OrdListSvc ordListSvc = new OrdListSvc();
+			
+			rcnt = ordListSvc.getOrdCount(buyer);	// 페이징을 위해 해당 회원의 전체 주문내역 수를 받아옴
+			ordList = ordListSvc.getOrdList(buyer, cpage, psize);	// 해당 회원의 전체 주문목록을 받아옴
+			
+			pcnt = rcnt / psize;
+			if (rcnt % psize > 0)	pcnt++;				// 전체 페이지수
+			spage = (cpage - 1) / psize * psize + 1;	// 블록 시작페이지 번호
+			epage = spage + psize - 1;
+			if (epage > pcnt)	epage = pcnt;			// 블록 종료페이지 번호
+			
+			OrdPageInfo pageInfo = new OrdPageInfo();	// 페이징에 필요한 정보를 담을 인스턴스 생성
+			pageInfo.setCpage(cpage);		// 현재 페이지 번호
+			pageInfo.setPcnt(pcnt);			// 전체 페이지 개수
+			pageInfo.setSpage(spage);		// 블록 시작페이지 번호
+			pageInfo.setEpage(epage);		// 블록 종료페이지 번호
+			pageInfo.setRcnt(rcnt);			// 전체 상품(레코드) 개수
+			pageInfo.setBsize(bsize);		// 블록내 페이지 개수
+			pageInfo.setPsize(psize);		// 페이지내 상품 개수
+			
+			request.setAttribute("ordList", ordList);
+			request.setAttribute("pageInfo", pageInfo);
+			forward.setPath("/member/order_list.jsp");
+		} else {	// 로그인해야만 들어올 수 있으므로
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('잘못된 경로로 들어오셨습니다.');");
+			out.println("history.back();");
+			out.println("</script>");
+			out.close();
+		}
+		
+		return forward;
+	}
+}
---
+++ fourplay/src/admin/controller/AOrdCtrl.java
@@ -0,0 +1,56 @@
+package admin.controller;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import action.Action;
+import admin.action.*;
+import vo.*;
+
+@WebServlet("*.orda")
+public class AOrdCtrl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public AOrdCtrl() {
+        super();
+    }
+    protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	request.setCharacterEncoding("utf-8");
+    	String requestUri = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestUri.substring(contextPath.length());
+		System.out.println(command);
+		
+		ActionForward forward = null;
+		Action action = null;
+//		HttpSession session = request.getSession();
+//		AdminInfo adminMember = (AdminInfo)session.getAttribute("adminMember");
+		
+		switch (command) {
+			case "/order/ord_list.orda" :		// 주문 목록 화면
+				action = new AOrdListAction();
+				break;
+		}
+		
+		try {
+			forward = action.execute(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (forward != null) {
+			if (forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			} else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+}
