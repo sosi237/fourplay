70855a83a5061fee21593e77c125a492b2a7f46b
soyeon <lupmika@naver.com>
2021-01-18 02:42:03 +0900

    faq

---
+++ fourplay/src/vo/FaqInfo.java
@@ -0,0 +1,50 @@
+package vo;
+
+public class FaqInfo {
+	private int fq_idx, fq_read, al_idx;
+	private String fq_writer, fq_title, fq_content, fq_date;
+	public int getFq_idx() {
+		return fq_idx;
+	}
+	public void setFq_idx(int fq_idx) {
+		this.fq_idx = fq_idx;
+	}
+	public int getFq_read() {
+		return fq_read;
+	}
+	public void setFq_read(int fq_read) {
+		this.fq_read = fq_read;
+	}
+	public int getAl_idx() {
+		return al_idx;
+	}
+	public void setAl_idx(int al_idx) {
+		this.al_idx = al_idx;
+	}
+	public String getFq_writer() {
+		return fq_writer;
+	}
+	public void setFq_writer(String fq_writer) {
+		this.fq_writer = fq_writer;
+	}
+	public String getFq_title() {
+		return fq_title;
+	}
+	public void setFq_title(String fq_title) {
+		this.fq_title = fq_title;
+	}
+	public String getFq_content() {
+		return fq_content;
+	}
+	public void setFq_content(String fq_content) {
+		this.fq_content = fq_content;
+	}
+	public String getFq_date() {
+		return fq_date;
+	}
+	public void setFq_date(String fq_date) {
+		this.fq_date = fq_date;
+	}
+	
+	
+}
