0f5120809e067bc1188543c395e4a2d60bcd191e
soyeon <lupmika@naver.com>
2021-01-18 23:36:26 +0900

    할인전 가격과 할인률 정보 포함

--- fourplay/src/dao/CartDao.java
+++ fourplay/src/dao/CartDao.java
@@ -13,7 +13,7 @@ public class CartDao {
    private CartDao() {}
    public static CartDao getInstance() {
       if (cartDao == null) {
-    	  cartDao = new CartDao();
+         cartDao = new CartDao();
       }
       return cartDao;
    }
@@ -21,175 +21,205 @@ public class CartDao {
       this.conn = conn;
    }
    public int cartInsert(CartInfo cart) {
-		// 사용자가 선택한 상품을 장바구니에 담는 메소드
-			int result = 0;
-			Statement stmt = null;
-			ResultSet rs = null;
+      // 사용자가 선택한 상품을 장바구니에 담는 메소드
+         int result = 0;
+         Statement stmt = null;
+         ResultSet rs = null;
 
-			try {
-				stmt = conn.createStatement();
-				String sql = "select cl_idx from t_cart_list " + 
-					" where cl_buyer = '" + cart.getCl_buyer() + "' " + 
-					" and cl_ismember = '" + cart.getCl_ismember() + "' " + 
-					" and pl_id = '" + cart.getPl_id() + "' " + 
-					" and cl_opt = '" + cart.getCl_opt() + "' ";
-				rs = stmt.executeQuery(sql);
-				if (rs.next()) {	// 장바구니의 기존 데이터 중 동일한 상품이 있을 경우
-					sql = "update t_cart_list set cl_cnt = cl_cnt + " + cart.getCl_cnt() + 
-						" where cl_idx = " + rs.getInt("cl_idx");
-				} else {	// 처음 장바구니에 담는 상품일 경우
-					sql = "insert into t_cart_list (cl_buyer, cl_ismember, pl_id, cl_opt, cl_cnt) " +
-					" values ('" + cart.getCl_buyer() + "', '" + cart.getCl_ismember() + "', '" + 
-					cart.getPl_id() + "', '" + cart.getCl_opt() + "', '" + cart.getCl_cnt() + "')";
-				}
+         try {
+            stmt = conn.createStatement();
+            String sql = "select cl_idx from t_cart_list " + 
+               " where cl_buyer = '" + cart.getCl_buyer() + "' " + 
+               " and cl_ismember = '" + cart.getCl_ismember() + "' " + 
+               " and pl_id = '" + cart.getPl_id() + "' " + 
+               " and cl_opt = '" + cart.getCl_opt() + "' ";
+            rs = stmt.executeQuery(sql);
+            if (rs.next()) {   // 장바구니의 기존 데이터 중 동일한 상품이 있을 경우
+               sql = "update t_cart_list set cl_cnt = cl_cnt + " + cart.getCl_cnt() + 
+                  " where cl_idx = " + rs.getInt("cl_idx");
+            } else {   // 처음 장바구니에 담는 상품일 경우
+               sql = "insert into t_cart_list (cl_buyer, cl_ismember, pl_id, cl_opt, cl_cnt) " +
+               " values ('" + cart.getCl_buyer() + "', '" + cart.getCl_ismember() + "', '" + 
+               cart.getPl_id() + "', '" + cart.getCl_opt() + "', '" + cart.getCl_cnt() + "')";
+            }
 
-				result = stmt.executeUpdate(sql);
+            result = stmt.executeUpdate(sql);
 
-			} catch(Exception e) {
-				System.out.println("cartInsert() 오류");		e.printStackTrace();
-			} finally {
-				close(rs);	close(stmt);
-			}
+         } catch(Exception e) {
+            System.out.println("cartInsert() 오류");      e.printStackTrace();
+         } finally {
+            close(rs);   close(stmt);
+         }
 
-			return result;
-		}   
-	public ArrayList<CartInfo> getCartList(String where) {
-		// 장바구니에서 보여줄 특정 사용자(회원, 비회원)의 장바구니 목록을 리턴하는 메소드
-		ArrayList<CartInfo> cartList = new ArrayList<CartInfo>();
-		Statement stmt = null;
-		ResultSet rs = null;
-		try {
-			String sql = "select c.cl_idx, p.pl_id, p.pl_name, p.pl_img1, " +
-					" p.pl_opt, c.cl_opt, c.cl_cnt, p.pl_price,p.pl_discount, s.ps_stock " + 
-					" from t_cart_list c , t_product_list p, t_product_size s" + 
-					" where c.pl_id = p.pl_id and p.pl_view = 'y' and s.ps_stock != 0 " + where + 
-					" group by p.pl_id order by p.pl_id, c.cl_opt";
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(sql);
-			while(rs.next()) {
-				CartInfo cart = new CartInfo();
-				cart.setCl_idx(rs.getInt("cl_idx"));
-				cart.setPl_id(rs.getString("pl_id"));
-				cart.setPl_name(rs.getString("pl_name"));
-				cart.setPl_img1(rs.getString("pl_img1"));
-				cart.setPl_opt(rs.getString("pl_opt"));
-				cart.setCl_opt(rs.getString("cl_opt"));
-				cart.setCl_cnt(rs.getInt("cl_cnt"));
-				cart.setPs_stock(rs.getInt("ps_stock"));
-				int price = rs.getInt("pl_price");	// 실 구매가
-				if (rs.getInt("pl_discount") > 0) {
-					float rate = (float)rs.getInt("pl_discount") / 100;
-					price = Math.round(price - (price * rate));
-				}	// 상품 가격은 할인율이 있을 경우 할인율을 적용한 가격으로 저장함
-				cart.setPrice(price);
-				cartList.add(cart);
-			}
-		} catch(Exception e) {
-			System.out.println("getCartList() 오류");		e.printStackTrace();
-		} finally {
-			close(rs);	close(stmt);
-		}
+         return result;
+      }   
+   public ArrayList<CartInfo> getCartList(String where) {
+      // 장바구니에서 보여줄 특정 사용자(회원, 비회원)의 장바구니 목록을 리턴하는 메소드
+      ArrayList<CartInfo> cartList = new ArrayList<CartInfo>();
+      Statement stmt = null;
+      ResultSet rs = null;
+      try {
+         String sql = "select c.cl_idx, p.pl_id, p.pl_name, p.pl_img1, " +
+               " p.pl_opt, c.cl_opt, c.cl_cnt, p.pl_price,p.pl_discount, s.ps_stock " + 
+               " from t_cart_list c , t_product_list p, t_product_size s" + 
+               " where c.pl_id = p.pl_id and p.pl_view = 'y' and s.ps_stock != 0 " + where + 
+               " group by p.pl_id order by p.pl_id, c.cl_opt";
+         stmt = conn.createStatement();
+         rs = stmt.executeQuery(sql);
+         while(rs.next()) {
+            CartInfo cart = new CartInfo();
+            cart.setCl_idx(rs.getInt("cl_idx"));
+            cart.setPl_id(rs.getString("pl_id"));
+            cart.setPl_name(rs.getString("pl_name"));
+            cart.setPl_img1(rs.getString("pl_img1"));
+            cart.setPl_opt(rs.getString("pl_opt"));
+            cart.setCl_opt(rs.getString("cl_opt"));
+            cart.setCl_cnt(rs.getInt("cl_cnt"));
+            cart.setPs_stock(rs.getInt("ps_stock"));
+            cart.setPl_price(rs.getInt("pl_price"));
+            
+            System.out.println("pl_price : "+cart.getPl_price());
+            
+            cart.setPl_discount(rs.getInt("pl_discount"));
+            int price = rs.getInt("pl_price");   // 실 구매가
+            if (rs.getInt("pl_discount") > 0) {
+               float rate = (float)rs.getInt("pl_discount") / 100;
+               price = Math.round(price - (price * rate));
+            }   // 상품 가격은 할인율이 있을 경우 할인율을 적용한 가격으로 저장함
+            cart.setPrice(price);
+            cartList.add(cart);
+         }
+      } catch(Exception e) {
+         System.out.println("getCartList() 오류");      e.printStackTrace();
+      } finally {
+         close(rs);   close(stmt);
+      }
 
-		return cartList;		
-	}
-	public int cartCntUpdate( String idx, String cnt, String buyer, String isMember) {
-		// 사용자가 선택한 상품의 수량을 변경하는 메소드
-			int result = 0;
-			Statement stmt = null;
+      return cartList;      
+   }
+   public int cartCntUpdate( String idx, String cnt, String buyer, String isMember) {
+      // 사용자가 선택한 상품의 수량을 변경하는 메소드
+         int result = 0;
+         Statement stmt = null;
 
-			try {
-				stmt = conn.createStatement();
-				String sql = "update t_cart_list set cl_cnt = '" + cnt + "' where cl_buyer = '" + 
-					buyer + "' and cl_ismember = '" + isMember + "' and cl_idx = " + idx;
+         try {
+            stmt = conn.createStatement();
+            String sql = "update t_cart_list set cl_cnt = '" + cnt + "' where cl_buyer = '" + 
+               buyer + "' and cl_ismember = '" + isMember + "' and cl_idx = " + idx;
 
-				result = stmt.executeUpdate(sql);
-			} catch(Exception e) {
-				System.out.println("cartCntUpdate() 오류");		e.printStackTrace();
-			} finally {
-				close(stmt);
-			}
+            result = stmt.executeUpdate(sql);
+         } catch(Exception e) {
+            System.out.println("cartCntUpdate() 오류");      e.printStackTrace();
+         } finally {
+            close(stmt);
+         }
 
-			return result;
-		}
+         return result;
+      }
+   public int cartDelete(String idx, String buyer, String isMember) {
+   // 사용자가 선택한 상품(들)을 장바구니에서 삭제하는 메소드
+      int result = 0;
+      Statement stmt = null;
+
+      try {
+         String[] arrIdx = idx.split(",");
+         String where = "";
+         for (int i = 0 ; i < arrIdx.length ; i++) {
+            where += " or cl_idx = " + arrIdx[i];
+         }
+         where = " and (" + where.substring(4) + ")";
+         String sql = "delete from t_cart_list where cl_buyer = '" + buyer + 
+            "' and cl_ismember = '" + isMember + "' " + where;
+         System.out.println(sql);
+         stmt = conn.createStatement();
+         result = stmt.executeUpdate(sql);
+      } catch(Exception e) {
+         System.out.println("cartDelete() 오류");      e.printStackTrace();
+      } finally {
+         close(stmt);
+      }
+
+      return result;
+   }
 
    public int wishInsert(CartInfo cart) {
-		// 사용자가 선택한 상품을 장바구니에 담는 메소드
-			int result = 0;
-			Statement stmt = null;
-			ResultSet rs = null;
+      // 사용자가 선택한 상품을 장바구니에 담는 메소드
+         int result = 0;
+         Statement stmt = null;
+         ResultSet rs = null;
 
-			try {
-				stmt = conn.createStatement();
-				String sql = "select wl_id from t_wish_list " + 
-					" where pl_id = '" + cart.getPl_id() + "' " +
-					" and ml_id = '" + cart.getMl_id() + "'";
-				rs = stmt.executeQuery(sql);
-				if (!rs.next()) {	// 처음 장바구니에 담는 상품일 경우
-					sql = "insert into t_wish_list (ml_id, pl_id) " +
-					" values ('" + cart.getMl_id() + "', '" + cart.getPl_id() +  "')";
-					result = stmt.executeUpdate(sql);
-				}
+         try {
+            stmt = conn.createStatement();
+            String sql = "select wl_id from t_wish_list " + 
+               " where pl_id = '" + cart.getPl_id() + "' " +
+               " and ml_id = '" + cart.getMl_id() + "'";
+            rs = stmt.executeQuery(sql);
+            if (!rs.next()) {   // 처음 장바구니에 담는 상품일 경우
+               sql = "insert into t_wish_list (ml_id, pl_id) " +
+               " values ('" + cart.getMl_id() + "', '" + cart.getPl_id() +  "')";
+               result = stmt.executeUpdate(sql);
+            }
 
-			} catch(Exception e) {
-				System.out.println("wishInsert() 오류");		e.printStackTrace();
-			} finally {
-				close(rs);	close(stmt);
-			}
+         } catch(Exception e) {
+            System.out.println("wishInsert() 오류");      e.printStackTrace();
+         } finally {
+            close(rs);   close(stmt);
+         }
 
-			return result;
-		} 
+         return result;
+      } 
    
    public int wishDelete(String id, String buyer) {
-		// 사용자가 선택한 상품(들)을 장바구니에서 삭제하는 메소드
-			int result = 0;
-			Statement stmt = null;
+      // 사용자가 선택한 상품(들)을 장바구니에서 삭제하는 메소드
+         int result = 0;
+         Statement stmt = null;
 
-			try {
-				String sql = "delete from t_wish_list where ml_id = '" + buyer + 
-					"' and wl_id = '" + id + "'";
+         try {
+            String sql = "delete from t_wish_list where ml_id = '" + buyer + 
+               "' and wl_id = '" + id + "'";
 
-				stmt = conn.createStatement();
-				result = stmt.executeUpdate(sql);
-			} catch(Exception e) {
-				System.out.println("wishDelete() 오류");		e.printStackTrace();
-			} finally {
-				close(stmt);
-			}
+            stmt = conn.createStatement();
+            result = stmt.executeUpdate(sql);
+         } catch(Exception e) {
+            System.out.println("wishDelete() 오류");      e.printStackTrace();
+         } finally {
+            close(stmt);
+         }
 
-			return result;
-		}
+         return result;
+      }
    
-	public ArrayList<CartInfo> getWishList(String where) {
-		// 장바구니에서 보여줄 특정 사용자(회원, 비회원)의 장바구니 목록을 리턴하는 메소드
-		ArrayList<CartInfo> wishList = new ArrayList<CartInfo>();
-		Statement stmt = null;
-		ResultSet rs = null;
-		try {
-			String sql = "select w.wl_id, p.pl_id, p.pl_name, p.pl_img1, p.pl_price, p.pl_discount" + 
-					" from t_wish_list w ,t_member_list m, t_product_list p " + 
-					" where w.ml_id = m.ml_id and w.pl_id = p.pl_id and p.pl_view = 'y' " + where + "'";
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(sql);
-			while(rs.next()) {
-				CartInfo wish = new CartInfo();
-				wish.setWl_id(rs.getInt("wl_id"));
-				wish.setPl_id(rs.getString("pl_id"));
-				wish.setPl_name(rs.getString("pl_name"));
-				wish.setPl_img1(rs.getString("pl_img1"));
-				int price = rs.getInt("pl_price");	// 실 구매가
-				if (rs.getInt("pl_discount") > 0) {
-					float rate = (float)rs.getInt("pl_discount") / 100;
-					price = Math.round(price - (price * rate));
-				}	// 상품 가격은 할인율이 있을 경우 할인율을 적용한 가격으로 저장함
-				wish.setPrice(price);
-				wishList.add(wish);
-			}
-		} catch(Exception e) {
-			System.out.println("getWishList() 오류");		e.printStackTrace();
-		} finally {
-			close(rs);	close(stmt);
-		}
+   public ArrayList<CartInfo> getWishList(String where) {
+      // 장바구니에서 보여줄 특정 사용자(회원, 비회원)의 장바구니 목록을 리턴하는 메소드
+      ArrayList<CartInfo> wishList = new ArrayList<CartInfo>();
+      Statement stmt = null;
+      ResultSet rs = null;
+      try {
+         String sql = "select w.wl_id, p.pl_id, p.pl_name, p.pl_img1, p.pl_price, p.pl_discount" + 
+               " from t_wish_list w ,t_member_list m, t_product_list p " + 
+               " where w.ml_id = m.ml_id and w.pl_id = p.pl_id and p.pl_view = 'y' " + where + "'";
+         stmt = conn.createStatement();
+         rs = stmt.executeQuery(sql);
+         while(rs.next()) {
+            CartInfo wish = new CartInfo();
+            wish.setWl_id(rs.getInt("wl_id"));
+            wish.setPl_id(rs.getString("pl_id"));
+            wish.setPl_name(rs.getString("pl_name"));
+            wish.setPl_img1(rs.getString("pl_img1"));
+            int price = rs.getInt("pl_price");   // 실 구매가
+            if (rs.getInt("pl_discount") > 0) {
+               float rate = (float)rs.getInt("pl_discount") / 100;
+               price = Math.round(price - (price * rate));
+            }   // 상품 가격은 할인율이 있을 경우 할인율을 적용한 가격으로 저장함
+            wish.setPrice(price);
+            wishList.add(wish);
+         }
+      } catch(Exception e) {
+         System.out.println("getWishList() 오류");      e.printStackTrace();
+      } finally {
+         close(rs);   close(stmt);
+      }
 
-		return wishList;		
-	}
+      return wishList;      
+   }
 }
--- fourplay/src/vo/CartInfo.java
+++ fourplay/src/vo/CartInfo.java
@@ -1,85 +1,99 @@
 package vo;
 
 public class CartInfo {
-	private int cl_idx, cl_cnt, ps_stock, price, wl_id;
-	private String cl_buyer, cl_ismember, pl_id, cl_opt, pl_name, pl_img1, pl_opt, ml_id ;
-	public int getCl_idx() {
-		return cl_idx;
-	}
-	public void setCl_idx(int cl_idx) {
-		this.cl_idx = cl_idx;
-	}
-	public int getCl_cnt() {
-		return cl_cnt;
-	}
-	public void setCl_cnt(int cl_cnt) {
-		this.cl_cnt = cl_cnt;
-	}
-	public int getPs_stock() {
-		return ps_stock;
-	}
-	public void setPs_stock(int ps_stock) {
-		this.ps_stock = ps_stock;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public int getWl_id() {
-		return wl_id;
-	}
-	public void setWl_id(int wl_id) {
-		this.wl_id = wl_id;
-	}
-	public String getCl_buyer() {
-		return cl_buyer;
-	}
-	public void setCl_buyer(String cl_buyer) {
-		this.cl_buyer = cl_buyer;
-	}
-	public String getCl_ismember() {
-		return cl_ismember;
-	}
-	public void setCl_ismember(String cl_ismember) {
-		this.cl_ismember = cl_ismember;
-	}
-	public String getPl_id() {
-		return pl_id;
-	}
-	public void setPl_id(String pl_id) {
-		this.pl_id = pl_id;
-	}
-	public String getCl_opt() {
-		return cl_opt;
-	}
-	public void setCl_opt(String cl_opt) {
-		this.cl_opt = cl_opt;
-	}
-	public String getPl_name() {
-		return pl_name;
-	}
-	public void setPl_name(String pl_name) {
-		this.pl_name = pl_name;
-	}
-	public String getPl_img1() {
-		return pl_img1;
-	}
-	public void setPl_img1(String pl_img1) {
-		this.pl_img1 = pl_img1;
-	}
-	public String getPl_opt() {
-		return pl_opt;
-	}
-	public void setPl_opt(String pl_opt) {
-		this.pl_opt = pl_opt;
-	}
-	public String getMl_id() {
-		return ml_id;
-	}
-	public void setMl_id(String ml_id) {
-		this.ml_id = ml_id;
-	}
-
+   private int cl_idx, cl_cnt, ps_stock, price, wl_id, pl_price, pl_discount;
+   //price : 할인율 적용된 가격, pl_price: 할인율 적용 전 가격, pl_discount: 할인율
+   private String cl_buyer, cl_ismember, pl_id, cl_opt, pl_name, pl_img1, pl_opt, ml_id ;
+   
+   public int getCl_idx() {
+      return cl_idx;
+   }
+   public void setCl_idx(int cl_idx) {
+      this.cl_idx = cl_idx;
+   }
+   public int getCl_cnt() {
+      return cl_cnt;
+   }
+   public void setCl_cnt(int cl_cnt) {
+      this.cl_cnt = cl_cnt;
+   }
+   public int getPs_stock() {
+      return ps_stock;
+   }
+   public void setPs_stock(int ps_stock) {
+      this.ps_stock = ps_stock;
+   }
+   public int getPrice() {
+      return price;
+   }
+   public void setPrice(int price) {
+      this.price = price;
+   }
+   public int getWl_id() {
+      return wl_id;
+   }
+   public void setWl_id(int wl_id) {
+      this.wl_id = wl_id;
+   }
+   public int getPl_price() {
+      return pl_price;
+   }
+   public void setPl_price(int pl_price) {
+      this.pl_price = pl_price;
+   }
+   public int getPl_discount() {
+      return pl_discount;
+   }
+   public void setPl_discount(int pl_discount) {
+      this.pl_discount = pl_discount;
+   }
+   public String getCl_buyer() {
+      return cl_buyer;
+   }
+   public void setCl_buyer(String cl_buyer) {
+      this.cl_buyer = cl_buyer;
+   }
+   public String getCl_ismember() {
+      return cl_ismember;
+   }
+   public void setCl_ismember(String cl_ismember) {
+      this.cl_ismember = cl_ismember;
+   }
+   public String getPl_id() {
+      return pl_id;
+   }
+   public void setPl_id(String pl_id) {
+      this.pl_id = pl_id;
+   }
+   public String getCl_opt() {
+      return cl_opt;
+   }
+   public void setCl_opt(String cl_opt) {
+      this.cl_opt = cl_opt;
+   }
+   public String getPl_name() {
+      return pl_name;
+   }
+   public void setPl_name(String pl_name) {
+      this.pl_name = pl_name;
+   }
+   public String getPl_img1() {
+      return pl_img1;
+   }
+   public void setPl_img1(String pl_img1) {
+      this.pl_img1 = pl_img1;
+   }
+   public String getPl_opt() {
+      return pl_opt;
+   }
+   public void setPl_opt(String pl_opt) {
+      this.pl_opt = pl_opt;
+   }
+   public String getMl_id() {
+      return ml_id;
+   }
+   public void setMl_id(String ml_id) {
+      this.ml_id = ml_id;
+   }
+   
 }
