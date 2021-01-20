2c255c3d9357c04a39511c5b653d1bc14ca70803
soyeon <lupmika@naver.com>
2021-01-21 02:30:25 +0900

    주석 수정

--- fourplay/src/action/WishDelAction.java
+++ fourplay/src/action/WishDelAction.java
@@ -6,24 +6,24 @@ import svc.*;
 import vo.*;
 
 public class WishDelAction implements Action {
-	// 장바구니에 선택한 상품정보를 저장하는 클래스
-		public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-			request.setCharacterEncoding("utf-8");
-			String id = request.getParameter("id");		// 위시리스트 아이디(들)
-			
-			String buyer;
-			HttpSession session = request.getSession();
-			MemberInfo loginMember = (MemberInfo)session.getAttribute("loginMember");
-			buyer = loginMember.getMlid();
+// 위시리스트의 상품(들)을 삭제하는 클래스
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		String id = request.getParameter("id");		// 위시리스트 아이디(들)
+		
+		String buyer;
+		HttpSession session = request.getSession();
+		MemberInfo loginMember = (MemberInfo)session.getAttribute("loginMember");
+		buyer = loginMember.getMlid();
 
 
-			response.setContentType("text/html; charset=utf-8");
-			PrintWriter out = response.getWriter();
-			
-			WishDelSvc wishDelSvc = new WishDelSvc();
-			int result = wishDelSvc.wishDelete(id, buyer);
-			out.println(result);
-			out.close();
-			return null; 
-		}
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		WishDelSvc wishDelSvc = new WishDelSvc();
+		int result = wishDelSvc.wishDelete(id, buyer);
+		out.println(result);
+		out.close();
+		return null; 
 	}
+}
